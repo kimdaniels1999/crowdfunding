cf409927d47310e2dbc4403203e38342f64b3bb3
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2021-11-07 22:06:04 +1000

    uninstall and reinstall npm

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route }from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
-import Nav from './components/Nav/Nav';
+import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
 
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 
 import Home from "./pages/HomePage";
-import SignInPage from './pages/SignInPage';
-import CategoriesPage from './pages/CategoriesPage';
-import HowItWorksPage from './pages/HowItWorksPage'
+import SignInPage from "./pages/SignInPage";
+import CategoriesPage from "./pages/CategoriesPage";
+import HowItWorksPage from "./pages/HowItWorksPage";
 import StartAProjectPage from "./pages/StartAProjectPage";
 import ProjectPage from "./pages/ProjectPage";
-
 
 const App = () => {
   return (
     <div>
-    <Router>
-      <Header />
-      <hr />
-      <Nav />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
+      <Router>
+        <Header />
+        <hr />
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
           </Route>
-        <Route exact path="/signin">
-          <SignInPage />
+          <Route exact path="/signin">
+            <SignInPage />
           </Route>
-        <Route path='/howitworks'>
-          <HowItWorksPage />
+          <Route path="/howitworks">
+            <HowItWorksPage />
           </Route>
-        <Route path='/categories'>
-          <CategoriesPage />
+          <Route path="/categories">
+            <CategoriesPage />
           </Route>
-        <Route path='/startaproject'>
-        <StartAProjectPage />
-        </Route>
-        <Route path='/project/:id'>
-        <ProjectPage />
-        </Route>
-      </Switch>
-    </Router>
-    <Footer />
+          <Route path="/startaproject">
+            <StartAProjectPage />
+          </Route>
+          <Route path="/project/:id">
+            <ProjectPage />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
     </div>
-    )
-}
+  );
+};
 
 export default App;
